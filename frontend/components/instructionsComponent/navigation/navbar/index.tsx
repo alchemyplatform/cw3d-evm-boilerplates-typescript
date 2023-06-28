c64e6b216509c9132d4cb6b9db56094d9016c48d
VItto Rivabella <72762629+Eversmile12@users.noreply.github.com>
2023-06-28 14:50:50 +0200

    Update index.tsx

--- frontend/components/instructionsComponent/navigation/navbar/index.tsx
+++ frontend/components/instructionsComponent/navigation/navbar/index.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-        <img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
+        <p>create-web3-dapp</p>
       </a>
       <ConnectKitButton />
     </nav>
